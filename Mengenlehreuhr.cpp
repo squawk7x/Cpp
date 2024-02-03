39973e7281787c1d2c6fedd10e573af71dd888be
squawk7x <andreas@ottburg.de>
2024-02-03 22:41:08 +0100

    Berliner Mengenlehreuhr

--- Mengenlehreuhr.cpp
+++ Mengenlehreuhr.cpp
@@ -3,92 +3,118 @@
 #include <ctime>
 #include <iomanip>
 #include <thread>
+#include <cstdlib>
+#include <string.h>
 
-// Function to convert a digits to its corresponding pattern
-std::string convertDigitToPattern(int digit, int row) {
-    std::string pattern;
-    switch (digit) {
-        case 0:
-            if (row == 3 || row == 5)
-                pattern = "░░░░░░░░░░░";
-            else
-                pattern = "░░ ░░ ░░ ░░";
-            break;
-        case 1:
-            if (row == 3 || row == 5)
-                pattern = "█░░░░░░░░░░";
-            else
-                pattern = "██ ░░ ░░ ░░";
-            break;
-        case 2:
-            if (row == 3 || row == 5)
-                pattern = "██░░░░░░░░░";
-            else
-                pattern = "██ ██ ░░ ░░";
-            break;
-        case 3:
+#ifdef _WIN32
+#define CLEAR_SCREEN "cls"
+#else
+#define CLEAR_SCREEN "clear"
+#endif
 
-            if (row == 3 || row == 5)
-                pattern = "███░░░░░░░░";
-            else
-                pattern = "██ ██ ██ ░░";
-            break;
-        case 4:
+// #define BASE 5
 
-            if (row == 3)
-                pattern = "████░░░░░░░";
-            else
-                pattern = "██ ██ ██ ██";
-            break;
-        case 5:
-            pattern = "█████░░░░░░";
-            break;
-        case 6:
-            pattern = "██████░░░░░";
-            break;
-        case 7:
-            pattern = "███████░░░░";
-            break;
-        case 8:
-            pattern = "████████░░░";
-            break;
-        case 9:
-            pattern = "█████████░░";
-            break;
-        case 10:
-            pattern = "██████████░";
-            break;
-        case 11:
-            pattern = "███████████";
-            break;
-        default:
-            pattern = "";
-            break;
+std::string makePattern(int digit, int length, std::string opaque,
+                        std::string translucent) {
+    std::string pattern = "";
+
+    for (int i = 0; i < digit; i++) {
+        pattern += opaque;
+    }
+
+    for (int i = digit; i < length; i++) {
+        pattern += translucent;
     }
 
     return pattern;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    int BASE = 5;
+
+    if (argc == 2 && (strcmp(argv[1], "1") == 0 || strcmp(argv[1], "2") == 0 ||
+                      strcmp(argv[1], "3") == 0 || strcmp(argv[1], "4") == 0 ||
+                      strcmp(argv[1], "5") == 0 || strcmp(argv[1], "6") == 0 ||
+                      strcmp(argv[1], "10") == 0 || strcmp(argv[1], "12") == 0)) {
+        BASE = std::atoi(argv[1]);
+    }
+
+    std::string pattern_H[24 / BASE + 1];
+    std::string pattern_L[BASE];
+    std::string pattern_S[60 / BASE];
+
+    std::string pos;
+    std::string pts;
+
+    if (BASE == 5) {
+        pos = "█";
+        pts = "░";
+    } else {
+        pos = "█ ";
+        pts = "░ ";
+    }
+    std::string pol;
+    std::string ptl;
+
+    if (BASE == 5) {
+        pol = "██ ";
+        ptl = "░░ ";
+    } else {
+        pol = "█ ";
+        ptl = "░ ";
+    }
+
+    // (BASE -1) +
+
+    for (int k = 0; k < 24 / BASE + 1; k++) {
+        pattern_H[k] = makePattern(k, 24 / BASE, pol, ptl);
+    }
+
+    for (int k = 0; k < 60 / BASE; k++) {
+        pattern_S[k] = makePattern(k, 60 / BASE - 1, pos, pts);
+    }
+
+    for (int k = 0; k < BASE; k++) {
+        pattern_L[k] = makePattern(k, BASE - 1, pol, ptl);
+    }
+
     while (true) {
-        // Get the current system time
+        system(CLEAR_SCREEN);
+
         auto currentTime = std::chrono::system_clock::to_time_t(
             std::chrono::system_clock::now());
 
-        // Convert the time to a tm structure for local time
         std::tm *localTime = std::localtime(&currentTime);
 
-        // Display the time in the Menenlehreuhr format
-        std::cout << "\n"
-                  << convertDigitToPattern((localTime->tm_hour / 5), 1) << "\n"
-                  << convertDigitToPattern((localTime->tm_hour % 5), 2) << "\n"
-                  << convertDigitToPattern(localTime->tm_min / 5, 3) << "\n"
-                  << convertDigitToPattern(localTime->tm_min % 5, 4) << "\n"
-                  << convertDigitToPattern(localTime->tm_sec / 5, 5) << "\n"
-                  << convertDigitToPattern(localTime->tm_sec % 5, 6) << "\n"
-                  << std::endl;
+        std::cout << "\n";
+        std::cout << pattern_H[localTime->tm_hour / BASE];
+        if (BASE == 5)
+            std::cout << "\n";
+        else
+            std::cout << "H ";
+        std::cout << pattern_L[localTime->tm_hour % BASE];
+        if (BASE == 5)
+            std::cout << "\n";
+        else
+            std::cout << ": ";
+        std::cout << pattern_S[localTime->tm_min / BASE];
+        if (BASE == 5)
+            std::cout << "\n";
+        else
+            std::cout << "M ";
+        std::cout << pattern_L[localTime->tm_min % BASE];
+        if (BASE == 5)
+            std::cout << "\n";
+        else
+            std::cout << ": ";
+        std::cout << pattern_S[localTime->tm_sec / BASE];
+        if (BASE == 5)
+            std::cout << "\n";
+        else
+            std::cout << "S ";
+        std::cout << pattern_L[localTime->tm_sec % BASE];
+        std::cout << "\n\n";
 
-        // Wait for one second before updating the time again
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
