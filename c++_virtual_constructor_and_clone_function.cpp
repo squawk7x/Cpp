7e912bcccf3468a3cdfe87eabe2bdf6522f90f14
squawk7x <andreas@ottburg.de>
2024-03-03 18:23:46 +0100

    files renamed and ordered

--- c++_virtual_constructor_and_clone_function.cpp
+++ c++_virtual_constructor_and_clone_function.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 class Dog {
 public:
     virtual Dog* clone() const {
-        return new Dog(*this);  // co-variant return type
+        return new Dog(*this); // co-variant return type
     }
 };
 
-class Yellowdog : public Dog {};
-virtual Yellowog* clone() const {
-    return new Yellowdog(*this);
-}
+class Yellowdog : public Dog {
+public:
+    virtual Yellowdog* clone() const override {
+        return new Yellowdog(*this);
+    }
+};
 
 void foo(Dog* d) { // d is a Yellowdog
     // Dog* c = new Dog(*d);   // c is a dog => not identical => clone()
-    Dog* c = new d->clone(); // c is a Yellowdog
+    Dog* c = d->clone(); // c is a Yellowdog
 }
 
 int main(int argc, char* argv[]) {
